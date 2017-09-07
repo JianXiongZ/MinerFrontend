35cca84279a1ccd7284da2acc433a95239a0bf7b
JianXiongZ <jianxiong@canaan-creative.com>
2017-09-07 19:34:46 +0800

    Add stop mining confirmation

--- js/miner_frontend.js
+++ js/miner_frontend.js
@@ -1,30 +1,31 @@
 $().ready(function(){
-            var radio_text = document.getElementById('pool-sel-ethpool').checked;
-            if (radio_text == true){
-                document.getElementById('mining_worker').innerHTML ='';
-            };
-            
     		$('#stop_mining').click(function(){
-    			$.ajax({
-    				type:"GET",
-    				url: "/eth_miner/stop_miner",
-    			});
-    		});
+                var r=confirm("确实停止挖矿吗?")
+                if (r==true)
+                {
+                    $.ajax({
+                    type:"GET",
+                    url: "/eth_miner/stop_miner.sh",
+                });
+                }
+    			
+    		})
     		$('#start_mining').click(function(){
     			$("#error").text("");
     			function get_config(){
-    				var myconfig=[];
-    				var val_eth=$('input:radio[name="pool-sel"]:checked').val();
-    				var val_dou=$('input:radio[name="poold-sel2"]:checked').val();
-    				if (val_eth == 'eth' ){
-    					myconfig.push($('#pool-cfg-poolurl').val(), $('#pool-cfg-wallet').val(), $('#pool-cfg-passwd').val())
-    				}
-    				else if (val_eth == 'f2' || val_eth == 'nano') {
-                        myconfig.push($('#pool-cfg-poolurl').val(), $('#pool-cfg-workername').val(), $('#pool-cfg-passwd').val(), $('#pool-cfg-wallet').val())
-    				}
-    				if (val_dou == 'decred' || val_dou == 'siacoin' || val_dou == 'lbry') {
-    					myconfig.push($('#poold-cfg-poolurl').val(), $('#poold-cfg-workername').val(), $('#poold-cfg-passwd').val(),$('#poold-cfg-wallet').val())
-    				}
+    			var myconfig=[];
+    			var val_eth=$('input:radio[name="pool-sel"]:checked').val();
+    			var val_dou=$('input:radio[name="poold-sel2"]:checked').val();
+				var checkbox_text=document.getElementById("dualmine-checkbox").checked
+    			if (val_eth == 'eth' ){
+    				myconfig.push(val_eth, $('#pool-cfg-poolurl').val(), $('#pool-cfg-wallet').val(), $('#pool-cfg-passwd').val())
+    			}
+    			else if (val_eth == 'f2' || val_eth == 'nano') {
+                    myconfig.push(val_eth, $('#pool-cfg-poolurl').val(), $('#pool-cfg-wallet').val(), $('#pool-cfg-passwd').val(), $('#pool-cfg-workername').val())
+    			}
+				if(checkbox_text == true){
+    					myconfig.push($('#poold-cfg-poolurl').val().replace(/\+/g, "%2B"), $('#poold-cfg-wallet').val(),$('#poold-cfg-passwd').val(), val_dou)    					
+				}
     				return myconfig;
     			}
     			$.ajax({
@@ -36,11 +37,11 @@ $().ready(function(){
     					if (data == "non_input") {
     						$("#error").text("Please input wallet or password or worker!");
     					}
-
     				}
     			});
     		});
     		$('#save_config').click(function(){
+				$("no_name").text("");
     			function save_config(){
     				var my_saveconfig = new Array();
     				chkitem=document.forms[1].chkitem;
@@ -56,23 +57,32 @@ $().ready(function(){
     					}
     				}
     			return my_saveconfig;
-    		}
+    			}
     			$.ajax({
     				type:"GET",
     				url:"/eth_miner/config.py?config_save=" + save_config(),
+				dataType: 'json',
+                    		success:function(data){
+                        		if (data == "no_name_password") {
+                            			$("#no_name").text("Please input name or password!");
+                        		}                       
+                    		}
     			});
     		});
     		$('#pool-sel-ethpool').click(function () {
                 document.getElementById('pool-cfg-poolurl').value='usl.ethpool.org:3333';
-                document.getElementById('mining_worker').innerHTML ='';
+				$('#pool-cfg-workername').hide();
+                $("[for='pool-cfg-workername']").hide();
             });
     		$('#pool-sel-f2pool').click(function () {
                 document.getElementById('pool-cfg-poolurl').value='eth.f2pool.com:8008';
-                document.getElementById('mining_worker').innerHTML ='<input id="pool-cfg-workername" type="text" class="slim-input"><label for="pool-cfg-workername">矿工名称</label>';
+				$('#pool-cfg-workername').show();
+                $("[for='pool-cfg-workername']").show();
             });
 			$('#pool-sel-nanopool').click(function () {
                 document.getElementById('pool-cfg-poolurl').value='eul.nanopool.org:9999';
-                document.getElementById('mining_worker').innerHTML ='<input id="pool-cfg-workername" type="text" class="slim-input"><label for="pool-cfg-workername">矿工名称</label>';
+				$('#pool-cfg-workername').show();
+                $("[for='pool-cfg-workername']").show();
 			});
 			$("#dualmine-checkbox").click(function(){
                 var _check = $(this).is(':checked');
@@ -112,18 +122,29 @@ $().ready(function(){
 			url:"/eth_miner/get_config.py",
 			dataType:"json",
 			success:function(data){
+				$("input[name=poold-sel2][value=" + data["dualpool"] + " ]").attr('checked',true);
+				$("input[name=pool-sel][value=" + data["onlypool"] + " ]").attr('checked',true);
 				$("#pool-cfg-poolurl").val(data["-epool"]);
 				$('#pool-cfg-passwd').val(data["-epsw"]);
 				$('#pool-cfg-wallet').val(data["-ewal"]);
 				$('#pool-cfg-workername').val(data["-eworker"]);
 				$('#poold-cfg-poolurl').val(data["-dpool"]);
-				$('#poold-cfg-workername').val(data["-dworker"]);
 				$('#poold-cfg-passwd').val(data["-dpasw"]);
 				$('#poold-cfg-wallet').val(data["-dwal"]);
-				
-				Materialize.updateTextFields();
+				$('#pool-cfg-uname').val(data["username"]);
+                $('#pool-cfg-pwd').val("");
+                if (data["ifrestart"] == "restart"){
+					document.getElementById("pool-sel-reboot").checked=true;
+            	}	
 
+            	var radio_text = document.getElementById('pool-sel-ethpool').checked;
+				if(radio_text == true){
+					$('#pool-cfg-workername').hide();
+            		$("[for='pool-cfg-workername']").hide();
+				}
+				Materialize.updateTextFields();
 
 			}
 		});
+	
   	});
